ca96d8aa4dd3bef4f5d064f66c99094658a85741
Sten Tijhuis <102481635+Stensel8@users.noreply.github.com>
2023-04-28 20:13:26 +0200

    Reworked code, fixed the installation bugs, Made the code run faster.

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -1,4 +1,4 @@
-﻿Write-Host ''
+Write-Host ''
 Write-Host ''
 Write-Host 'Installatiebestanden binnenhalen van Adobe server(s)...'
 
@@ -14,7 +14,18 @@ $webclient.DownloadFile($url, $outputfile)
 
 Write-Host ".EXE bestand uitpakken naar losse bestanden..."
 
-& "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\AcroRdrDCx642300120143_nl_NL.exe" -sfx_o"$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup" -sfx_ne
+Start-Process -FilePath "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\AcroRdrDCx642300120143_nl_NL.exe" -ArgumentList "-sfx_o`"$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup`" -sfx_ne -quiet" -Wait
+
+
+Write-Host ''
+Write-Host ''
+Write-Host 'Customization Wizard binnenhalen van Adobe server(s)...'
+$webclient2 = New-Object System.Net.WebClient
+$url2 = "https://ardownload3.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/CustWiz2200320310_en_US_DC.exe"
+$workingdir = "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup"
+$outputfile2 = "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\Customization Wizard 2200320310.exe"
+$webclient2.DownloadFile($url2, $outputfile2)
+
 
 Write-Host "Alle benodigde installatiebestanden voor Adobe Acrobat zijn gedownload naar $workingdir."
 Start-Sleep -Seconds 5
