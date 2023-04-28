a0b991803c2fd70548ab4a9a102fdf05902e4835
Sten Tijhuis <102481635+Stensel8@users.noreply.github.com>
2023-04-28 20:57:37 +0200

    Added Adobe CustWizard.

--- Scripts/AdobeReaderMSIPrep64bit.ps1
+++ Scripts/AdobeReaderMSIPrep64bit.ps1
@@ -1,22 +1,46 @@
 ﻿Write-Host ''
+Write-Host '->  Installatiebestanden binnenhalen van Adobe server(s)...'
 Write-Host ''
-Write-Host 'Installatiebestanden binnenhalen van Adobe server(s)...'
 
-New-Item -ItemType Directory "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\" -Force
+New-Item -ItemType Directory "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\" -Force | Out-Null
 Set-Location "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\"
 
 $webclient = New-Object System.Net.WebClient
 $url = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/2300120143/AcroRdrDCx642300120143_nl_NL.exe"
-$workingdir = "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup"
 $outputfile = "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\AcroRdrDCx642300120143_nl_NL.exe"
 $webclient.DownloadFile($url, $outputfile)
 
-
 Write-Host ".EXE bestand uitpakken naar losse bestanden..."
+Start-Process -FilePath "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\AcroRdrDCx642300120143_nl_NL.exe" -ArgumentList "-sfx_o`"$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup`" -sfx_ne -quiet" -Wait
+Write-Host ".EXE bestand uitgepakt."
 
-& "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\AcroRdrDCx642300120143_nl_NL.exe" -sfx_o"$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup" -sfx_ne
 
-Write-Host "Alle benodigde installatiebestanden voor Adobe Acrobat zijn gedownload naar $workingdir."
-Start-Sleep -Seconds 5
-Write-Host "Script zal nu afsluiten."
+Write-Host ''
+Write-Host ''
+Write-Host '->  Customization Wizard binnenhalen van Adobe server(s)...'
+$webclient2 = New-Object System.Net.WebClient
+$url2 = "https://ardownload3.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/CustWiz2200320310_en_US_DC.exe"
+$outputfile2 = "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\Customization Wizard 2200320310.exe"
+$webclient2.DownloadFile($url2, $outputfile2)
+
 Start-Sleep -Seconds 3
+
+Write-Host '->  Microsoft Intune Win32 Content Prep Tool binnenhalen van GitHub server(s)...'
+Write-Host ''
+$webclient3 = New-Object System.Net.WebClient
+$url3 = "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/archive/refs/heads/master.zip"
+$outputfile3 = "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\Microsoft Win32 Content Prep Tool.zip"
+$webclient3.DownloadFile($url3, $outputfile3)
+
+Write-Host "Uitpakken van Microsoft Win32 Content Prep Tool..."
+Expand-Archive -Path "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\Microsoft Win32 Content Prep Tool.zip" -DestinationPath "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\Intune"
+Remove-Item -Path "$env:USERPROFILE\Downloads\Adobe-Acrobat-Setup\Microsoft Win32 Content Prep Tool.zip"
+Write-Host ".ZIP uitgepakt."
+
+Start-Sleep -Seconds 5
+Write-Host ''
+Write-Host ''
+Write-Host "Alle benodigde installatiebestanden om Adobe Acrobat te packagen in Microsoft Intune zijn gedownload naar $workingdir."
+Start-Sleep -Seconds 2
+Write-Host "Script zal nu afsluiten."
+Start-Sleep -Seconds 5
