9ae1bcb183969a63e982f57f25e414ecb53ffee1
Sten Tijhuis <102481635+Stensel8@users.noreply.github.com>
2023-05-02 23:34:11 +0200

    Uitproberen van dingen.

--- Scripts/Nieuw Hugo, moet nog extra apps toevoegen.ps1
+++ Scripts/Nieuw Hugo, moet nog extra apps toevoegen.ps1
@@ -6,21 +6,23 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 
   Write-Host "Menu:"
-Write-Host "1. Choice 1"
-Write-Host "2. Choice 2"
-Write-Host "3. Choice 3"
-$choice = Read-Host "Enter your choice"
+Write-Host "1. Choice 1: Google Chrome (64-bit)"
+Write-Host "2. Choice 2: Adobe Acrobat Reader DC - Default image (64-bit)"
+#Write-Host "3. Choice 3: Adobe Acrobat Reader DC - Customized image (64-bit)"
+#Write-Host "4. Choice 4: Citrix Workspace (64-bit)"
+$choice = Read-Host "Enter the number of choice"
 
 switch ($choice) {
     "1" {
-        # Execute code for Choice 1
-        Write-Host "You chose Choice 1"
+        # Execute the code for Choice 1
+        Write-Host "You have choosen option 1..."
+        Start-Sleep -seconds 2
    
     $intunePath = "$env:SystemDrive\Intune"
     if  (Test-Path $intunePath) {
-     Write-Host "Folder already exists."
+     Write-Host "Folder already exists... Using that folder."
     } 
-    else {
+    else { 
        New-Item -ItemType directory -Path $intunePath
   
     }
@@ -28,15 +30,18 @@ switch ($choice) {
 
     $ZipFile = "$intunePath\temp.zip"
     New-Item $ZipFile -ItemType File -Force
-
+    
     $RepositoryZipUrl = "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/archive/refs/heads/master.zip" 
-    # download the zip 
+    # Download the .zip file
     Write-Host 'Starting downloading the GitHub Repository'
-    Invoke-WebRequest  -Uri $RepositoryZipUrl -OutFile $ZipFile
-    Write-Host 'Download finished'
+    Start-BitsTransfer -Source $RepositoryZipUrl -Destination $ZipFile
+    Write-Host 'Download finished, now beginning to package the application...'
+    Start-Sleep -seconds 2
+    
 
     
-    #Extract Zip File
+    
+    # Extract the .zip file
     Write-Host 'Starting unzipping the GitHub Repository locally'
     Expand-Archive -Path $ZipFile -DestinationPath $intunePath -Force
     Write-Host 'Unzip finished'
@@ -45,21 +50,25 @@ switch ($choice) {
     Copy-Item -Path C:\Intune\Microsoft-Win32-Content-Prep-Tool-master\* -Destination $intunePath -PassThru
     Remove-Item -Path C:\Intune\Microsoft-Win32-Content-Prep-Tool-master\ -Force -Recurse
 
-    $sourcePath = "$env:SystemDrive\Intune\GoogleChrome\Source"
-    $outputPath = "$env:SystemDrive\Intune\GoogleChrome\Output"
-    if ( (Test-Path $sourcePath) -or (Test-Path $outputPath))  {
+    $ChromeSourcePath = "$env:SystemDrive\Intune\GoogleChrome\Source"
+    $ChromeOutputPath = "$env:SystemDrive\Intune\GoogleChrome\Output"
+    if ( (Test-Path $ChromeSourcePath) -or (Test-Path $ChromeOutputPath))  {
         Write-Host "Folder already exists."
     } 
     else {
-        New-Item -ItemType directory -Path $sourcePath
-        New-Item -ItemType directory -Path $outputPath
+        New-Item -ItemType directory -Path $ChromeSourcePath
+        New-Item -ItemType directory -Path $ChromeOutputPath
     }
 
      
-	$webclient = New-Object System.Net.WebClient
     $Link = "http://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi" 
     $fileName = "$env:SystemDrive\Intune\GoogleChrome\Source\GoogleChromeStandaloneEnterprise64.msi"
-    $webclient.DownloadFile($Link, $fileName)
+    
+    Write-Host 'Starting downloading the Google Chrome MSI'
+    Start-BitsTransfer -Source $Link -Destination $fileName
+    Write-Host 'Download finished, now beginning to package the application...'
+    Start-Sleep -seconds 2
+    
 
 
     Start-Process C:\Intune\IntuneWinAppUtil.exe -ArgumentList "-c `"$sourcePath`" -s GoogleChromeStandaloneEnterprise64.msi -o `"$outputPath`""
@@ -68,24 +77,24 @@ switch ($choice) {
      }
     "2" {
         # Execute code for Choice 2
-        Write-Host "You chose Choice 2"
-
+        Write-Host "You have choosen option 2..."
          $intunePath = "$env:SystemDrive\Intune"
+
     if  (Test-Path $intunePath) {
-     Write-Host "Folder already exists."
-    } 
+     Write-Host "Folder already exists... Using that folder."
+    }
+    
     else {
        New-Item -ItemType directory -Path $intunePath
   
     }
 
-
     $ZipFile = "$intunePath\temp.zip"
     New-Item $ZipFile -ItemType File -Force
 
     $RepositoryZipUrl = "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/archive/refs/heads/master.zip" 
-    # download the zip 
-    Write-Host 'Starting downloading the GitHub Repository'
+    # download the .zip 
+    Write-Host 'Preparing to download the GitHub Repository'
     Invoke-WebRequest  -Uri $RepositoryZipUrl -OutFile $ZipFile
     Write-Host 'Download finished'
 
@@ -102,7 +111,7 @@ switch ($choice) {
     $sourcePath = "$env:SystemDrive\Intune\Adobe\Source"
     $outputPath = "$env:SystemDrive\Intune\Adobe\Output"
     if ( (Test-Path $sourcePath) -or (Test-Path $outputPath))  {
-        Write-Host "Folder already exists."
+        Write-Host "Folder already exists.... Using that folder."
     } 
     else {
         New-Item -ItemType directory -Path $sourcePath
@@ -110,23 +119,46 @@ switch ($choice) {
     }
 
      
-	$webclient = New-Object System.Net.WebClient
-    $Link = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2300120064/AcroRdrDC2300120064_nl_NL.exe" 
-    $fileName = "$env:SystemDrive\Intune\Adobe\Source\AcroRdrDC2300120064_nl_NL.exe"
-    $webclient.DownloadFile($Link, $fileName)
+    $sourceUrl = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/2300120064/AcroRdrDCx642300120064_nl_NL.exe"
+    $fallbackUrl = "https://ardownload3.adobe.com/pub/adobe/acrobat/win/AcrobatDC/2300120064/AcroRdrDCx642300120064_nl_NL.exe" 
+    $fileName = "$env:SystemDrive\Intune\Adobe\Source\AcroRdrDCx642300120064_nl_NL.exe"
+    
+     # Download the file using the primary URL
+  Write-Output "Downloading Adobe Acrobat Reader DC..."
+  try {
+      Start-BitsTransfer -Source $sourceUrl -Destination $fileName -ErrorAction
+  } catch {
+      Write-Output "Failed to download from primary server: $($_.Exception.Message)"
+      Write-Output "Downloading Adobe Acrobat Reader DC from fallback server..."
+      try {
+          Start-BitsTransfer -Source $fallbackUrl -Destination $fileName -ErrorAction Stop
+      } catch {
+          Write-Output "Failed to download from fallback server, are the downloadservers up-and-running?: $($_.Exception.Message)"
+          exit 1
+      }
+  }
 
+    Write-Host 'Download finished, now beginning to package the application...'
+    Start-Sleep -seconds 2
 
-    Start-Process C:\Intune\IntuneWinAppUtil.exe -ArgumentList "-c `"$sourcePath`" -s AcroRdrDC2300120064_nl_NL.exe -o `"$outputPath`""
+    Start-Process C:\Intune\IntuneWinAppUtil.exe -ArgumentList "-c `"$sourcePath`" -s AcroRdrDCx642300120064_nl_NL.exe -o `"$outputPath`"" -Wait
 
 
     }
-    "3" {
-        # Execute code for Choice 3
-        Write-Host "You chose Choice 3"
+    "4" {
+        # Execute code for Choice 4
+        Write-Host "You have choosen option 4..."
+
     }
     Default {
         # Execute code if the user enters an invalid choice
         Write-Host "Invalid choice"
     } 
 }
+
+for ($i=0; $i -lt 5; $i++) {
+    Write-Host ""
+}
+
+$null = Read-Host "Press Enter to end the script."
 
